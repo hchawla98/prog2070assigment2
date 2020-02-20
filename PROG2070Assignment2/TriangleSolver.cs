69f97c51f5a41997af50b4edecbb87b4a827cb71
Hritik Chawla <chawla.hritik2011@gmail.com>
2020-02-20 00:00:58 -0500

    TriangleSolver class completed

---
+++ PROG2070Assignment2/TriangleSolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assignment2
+{
+    class TriangleSolver
+    {
+        string output;
+        public static string Analyze(int a, int b, int c)
+        {
+            if((a+b> c) && (a+c > b) && (b+c > a))
+            {
+                Console.WriteLine("Triangle is formed");
+
+                if(a == b && b == c)
+                {
+                    Console.WriteLine("It is an equilateral triangle");
+                }
+                else if(a==b || b==c || a == c)
+                {
+                    Console.WriteLine("It is an isosceles triangle");
+                }
+                else
+                {
+                    Console.WriteLine("It is a scalene triangle");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Triangle cannot be formed");
+            }
+            return "-------------------------";
+        }
+    }
+}
