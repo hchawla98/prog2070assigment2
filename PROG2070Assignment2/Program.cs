2770e6958037035b6c2c183169fd053db8a6702b
Hritik Chawla <chawla.hritik2011@gmail.com>
2020-02-20 01:15:03 -0500

    completed console application

--- PROG2070Assignment2/Program.cs
+++ PROG2070Assignment2/Program.cs
@@ -6,10 +6,97 @@ using System.Threading.Tasks;
 
 namespace PROG2070Assignment2
 {
+    
     class Program
     {
+        public static int choice, a, b, c;
         static void Main(string[] args)
         {
+            START:
+            Menu();
+            if(choice == 1)
+            {
+                sideone();
+                sidetwo();
+                sidethree();
+                TriangleSolver.Analyze(a, b, c);
+                goto START;
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
+            Console.ReadLine();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Enter the number corresponding to your choice and hit enter");
+            Console.WriteLine("1. Enter triangle dimensions");
+            Console.WriteLine("2. Exit");
+
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+                Menu();
+            }
+
+            if(choice != 1 && choice != 2)
+            {
+                Console.WriteLine("Invalid choice");
+                Menu();
+            }
+            
+
+        }
+        public static void sideone()
+        {
+            Console.WriteLine("Enter the value of first side");
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value");
+                sideone();
+            }
+            
+            
+        }
+        public static void sidetwo()
+        {
+            Console.WriteLine("Enter the value of second side");
+            try
+            {
+                b = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value");
+                sidetwo();
+            }
+
+
+        }
+        public static void sidethree()
+        {
+            Console.WriteLine("Enter the value of third side");
+            try
+            {
+                c = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value");
+                sidethree();
+            }
+
+
         }
     }
 }
